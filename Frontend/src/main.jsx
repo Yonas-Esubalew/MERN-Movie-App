b5008612730efa9a131151be843e3e7c8e7d8070
yonas pro <yonasbe999@gmail.com>
2025-04-08 04:37:57 -0900

    finally finished one

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { App } from "./App.jsx";
 import "./index.css";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
-import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { Navigate, Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home.jsx";
 //auth
@@ -40,6 +40,7 @@ const router = createBrowserRouter(
         <Route path="/admin/movies/update/:id" element={<UpdateMovie />} />
         <Route path="/admin/movies/comments" element={<AllComments />} />
         <Route path="/admin/movies/dashboard" element={<AdminDashboard />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Route>
   )
